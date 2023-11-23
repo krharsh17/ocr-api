7d032837d313bf7ccd231ee29741dc26c45847f0
Kumar Harsh <kharsh39@gmail.com>
2023-11-23 14:08:37 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from PIL import Image
+import pytesseract
+
+app = Flask(__name__)
+
+@app.route('/ocr', methods=['POST'])
+def ocr_process():
+    if request.method == 'POST':
+        image_file = request.files['image']
+        image_data = Image.open(image_file)
+
+        # Perform OCR using PyTesseract
+        text = pytesseract.image_to_string(image_data)
+
+        response = {
+            'status': 'success',
+            'text': text
+        }
+
+        return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
